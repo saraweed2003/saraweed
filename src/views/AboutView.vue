41727fb6c2b3ad8aab040c35a0be8db2549ad426
sarawut.2003 <sarawutsriprachai@gmail.com>
2023-08-23 23:29:21 +0700

    .

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,15 +4,16 @@
         <p class="sm:text-center text-justify sm:w-4/12 mx-auto dark:text-gray-300">
           <div>
        <div class="text-[10px] text-black dark:text-gray-500 pt-[10px]">Subscribe</div>
-       <h1><a href="https://www.youtube.com/@SARAWEED" class="dark:text-white text-gray-600 hover:text-black"> SARAWEED OFFICIAL </a></h1>
+       <h1><a href="https://www.youtube.com/@SARAWEED" target="_blank" class="dark:text-white text-gray-600 hover:text-black"> SARAWEED OFFICIAL </a></h1>
 
        <div class="text-[10px] text-black dark:text-gray-500 pt-[10px]">Platform Streaming</div>
-        <h1><a href="https://bfan.link/SWP2003" class="dark:text-white text-gray-600 hover:text-black"> SARAWEED </a></h1>
+        <h1><a href="https://bfan.link/SWP2003" target="_blank" class="dark:text-white text-gray-600 hover:text-black"> SARAWEED </a></h1>
 
          <div class="text-[10px] text-black dark:text-gray-500 pt-[10px]">Produce Beats</div>
      <h1>
       <a
         href="https://youtube.com/playlist?list=PLSjRxfegiMSH4jFqUAgOnsMzWVnnmPHnw" class="dark:text-white text-gray-600 hover:text-black"
+        target="_blank"
         >SWP BEATZ
       </a>
     </h1>
